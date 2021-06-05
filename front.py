9a72e2596355e526d0b609b375e867c5e5091bab
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-06-05 22:43:31 +0900

    Test uploading with real IP address

--- front.py
+++ front.py
@@ -1,14 +1,14 @@
 import sys
 from os import path
 
-from flask import Blueprint, render_template, abort, request, redirect, flash, current_app
+from flask import Blueprint, render_template, abort, request, redirect, flash, current_app, url_for
 from jinja2 import TemplateNotFound
 from werkzeug.utils import secure_filename
 
 
 front = Blueprint('front', __name__, template_folder='templates')
 
-ALLOWED_EXTENSIONS = {'txt', 'json'}
+ALLOWED_EXTENSIONS = {'txt', 'json', 'md'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -30,14 +30,16 @@ def upload_file():
 
 
         myfiles = request.files.to_dict(flat=False)['file']
-        # print(f'File result : {}', file=sys.stderr)
+        new_flie_list = []
+        print(f'File result : {myfiles}', file=sys.stderr)
         for f in myfiles:
             if f.filename == '':
                 flash('No selected file')
                 return redirect(request.url)
             if f and allowed_file(f.filename):
                 filename = secure_filename(f.filename)
+                new_flie_list.append(filename)
                 f.save(path.join(current_app.config['UPLOAD_FOLDER'], filename))
-        return render_template('upload.html')
+        return f'Upload Success : {new_flie_list}'
     elif request.method == 'GET':
         return render_template('upload.html')
